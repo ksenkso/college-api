52d7cd3c67884e38ffaa318608deead6cca3bba8
ksenkso <yazunya@gmail.com>
2017-05-06 20:37:14 +0300

    Initial commit

---
+++ common/config/main.php
@@ -0,0 +1,21 @@
+<?php
+return [
+    'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'components' => [
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'enableStrictParsing' => false,
+            'rules' => [
+                [
+                    'class' => 'yii\rest\UrlRule',
+                    'controller' => 'api/user'
+                ],
+            ],
+        ],
+    ],
+
+];
---
+++ frontend/modules/v1.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace frontend\modules;
+
+/**
+ * v1 module definition class
+ */
+class v1 extends \yii\base\Module
+{
+    /**
+     * @inheritdoc
+     */
+    public $controllerNamespace = 'frontend\modules\controllers';
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
